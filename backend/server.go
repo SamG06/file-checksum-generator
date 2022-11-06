34c9d73f4655ed49c1279dfe6f010887ca8abb90
SamG <16375795+SamG06@users.noreply.github.com>
2022-11-06 18:28:26 -0500

    initial setup

---
+++ backend/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "Hello!")
+	})
+
+    fmt.Printf("Starting server at port 3000\n")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+        log.Fatal(err)
+    }
+}
