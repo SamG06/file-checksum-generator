5cb624eff351d499f44cd5e71582061cb43a6e85
SamG <16375795+SamG06@users.noreply.github.com>
2022-11-08 20:55:10 -0500

    useless interface

--- frontend/src/stores/server.ts
+++ frontend/src/stores/server.ts
@@ -1,9 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-interface useServerStore {
-  serverAddress: String;
-}
 export const useServerStore = defineStore("server", () => {
   const serverAddress = ref("http://localhost:3000/");
 
