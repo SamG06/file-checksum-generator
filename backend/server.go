193fda0583d1e0836e9feff88ce67ef4a3f89c4a
SamG <16375795+SamG06@users.noreply.github.com>
2022-11-09 22:48:24 -0500

    no more snake case, removed redundant code

--- backend/server.go
+++ backend/server.go
@@ -18,22 +18,20 @@ func main() {
 		file, fileHeader, err := r.FormFile("file")
 
 		fmt.Printf("File name is %s\n", fileHeader.Filename)
-		
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 
-		// greeting := "Hello!"
+		fileHash := sha512.New()
 
-		file_hash := sha512.New()
-
-		if _,err := io.Copy(file_hash, file); err != nil {
+		if _,err := io.Copy(fileHash, file); err != nil {
 			log.Fatal(err)
 		}
 
-		hash := file_hash.Sum(nil)
+		hash := fileHash.Sum(nil)
 
-		fmt.Fprint(w, hex.EncodeToString(hash[:]))
+		fmt.Fprint(w, hex.EncodeToString(hash))
 	})
 
     fmt.Printf("Starting server at port 3000\n")
